e0d017de2f98743a3df4514ccc7de1b977f98e88
ssy828 <828yss@gmail.com>
2017-11-26 02:03:29 +0900

    add TextField Extension

--- ForestClone/ForestClone/Splash/SplashViewController.swift
+++ ForestClone/ForestClone/Splash/SplashViewController.swift
@@ -1,25 +1,19 @@
 //
 //  ViewController.swift
 //  ForestClone
-//
-//  Created by SIMA on 2017. 11. 25..
-//  Copyright © 2017년 ForestClone. All rights reserved.
-//
 
 import UIKit
 
 class SplashViewController: UIViewController {
 
+//    @IBOutlet weak var logoIMG: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+ 
 
 }
 

39d46495d946d4e55d923ba70fc7b2a167ae6c3f
simajune <tejayjung@gmail.com>
2017-11-25 22:53:19 +0900

    add Podfile, complete foldering

---
+++ ForestClone/ForestClone/Splash/SplashViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForestClone
+//
+//  Created by SIMA on 2017. 11. 25..
+//  Copyright © 2017년 ForestClone. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ForestClone/Pods/Target Support Files/Pods-ForestClone/Pods-ForestClone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForestClone : NSObject
+@end
+@implementation PodsDummy_Pods_ForestClone
+@end
